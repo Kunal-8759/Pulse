dd759566fca6ab8d49d7be35baf80dd03700b451
Kunal Kumar <kumarkunal8759@gmail.com>
2025-05-01 16:38:42 +0530

    fetched the data from the backend

---
+++ Frontend/src/services/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+export const fetchContests = () => {
+  return axios.get('http://localhost:5000/api/contests');
+};
+
+export const fetchHackathons = ()=>{
+  return axios.get('http://localhost:5000/api/hackathons');
+}
+
+export const getGitHubHeatmap =  (username) =>{
+  const data =axios.get(`http://localhost:5000/api/heatmap/github/${username}`);
+  console.log("GitHub Heatmap Data:", data);
+  return data;
+}
+
+export const getLeetCodeData =  (username) =>{
+  return axios.get(`http://localhost:5000/api/heatmap/leetcode/${username}`);
+}
+
+export const getDailyProblem= ()=>{
+  return axios.get('http://localhost:5000/api/heatmap/dailyProblem');
+}
+
